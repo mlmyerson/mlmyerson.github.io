b4dace6fdc4671935e206668dffba5ffa90741f2
mlmyerson <mlmyerson@gmail.com>
2025-04-08 18:04:44 -0400

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mlmyerson.github.io/',
+  base: '/',  // For username.github.io repos, use '/'
 })
